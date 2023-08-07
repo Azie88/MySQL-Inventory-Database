89c07094516d759222e8de9eeffd332b7e4b68c2
Azie88 <obandoandrew8@gmail.com>
2023-08-07 16:17:49 +0300

    SQL queries

--- mySQL.sql
+++ mySQL.sql
@@ -1,11 +1,11 @@
 --  1. Average price of items with physical inventory and those without.
 
 SELECT 
-    item.has_physical_inventory,
-    AVG(item_price.price) AS average_price
+    has_physical_inventory,
+    AVG(price) AS average_price
 FROM item
 INNER JOIN item_price ON item.default_price_id = item_price.item_price_id
-GROUP BY item.has_physical_inventory;
+GROUP BY has_physical_inventory;
 
 -- 2. Show which storeroom has the most inventory.
 
@@ -30,10 +30,10 @@ FROM item_price;
 SELECT 
     item.item_id, 
     item.item_name, 
-    item_stock.quantity
+    quantity
 FROM item_stock
 INNER JOIN item ON item_stock.item_id = item.item_id
-WHERE item_stock.quantity > 0 AND item_stock.quantity < 10;
+WHERE quantity > 0 AND quantity < 10;
 
 -- 5. List items whose quantity is Zero (0) and how long (In minutes) they have been in this state.
 --  From last date changed to current date timestamp
