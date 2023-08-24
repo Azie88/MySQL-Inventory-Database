d2ce9d2bea9da8f1ef16c33b92abe0e0dbad0d33
Azie88 <obandoandrew8@gmail.com>
2023-08-24 12:56:38 +0300

    updated code

--- mySQL.sql
+++ mySQL.sql
@@ -15,14 +15,17 @@ SELECT
     SUM(item_stock.quantity) AS total_quantity
 FROM item_stock
 INNER JOIN stockroom ON item_stock.stockroom_id = stockroom.stockroom_id
-GROUP BY item_stock.stockroom_id, stockroom_name;
+GROUP BY item_stock.stockroom_id, stockroom_name
+ORDER BY total_quantity DESC
+LIMIT 1;
 
 -- 3. Time in minutes since item prices were last modified.
--- Time of last item modified in item_prices table
+-- Difference between time of last item created vs. modified in item_prices table
 
 SELECT 
     MAX(date_changed) AS last_date_changed,
-    TIMESTAMPDIFF(MINUTE, MAX(date_changed), NOW()) AS minutes_since_last_change
+    MAX(date_created) AS last_date_created,
+    TIMESTAMPDIFF(MINUTE, MAX(date_created), MAX(date_changed)) AS minutes_since_last_change
 FROM item_price;
 
 -- 4. List items that are running out of stock.
@@ -33,7 +36,8 @@ SELECT
     quantity
 FROM item_stock
 INNER JOIN item ON item_stock.item_id = item.item_id
-WHERE quantity > 0 AND quantity < 10;
+WHERE quantity > 0 AND quantity < 10
+ORDER BY quantity DESC;
 
 -- 5. List items whose quantity is Zero (0) and how long (In minutes) they have been in this state.
 --  From last date changed to current date timestamp
@@ -44,7 +48,17 @@ SELECT
     item_stock.quantity,
     date_created,
     date_changed,
-    TIMESTAMPDIFF(MINUTE, date_changed, NOW()) AS minutes_in_zero
+    TIMESTAMPDIFF(MINUTE, date_created, date_changed) AS minutes_in_zero
 FROM item_stock
 INNER JOIN item ON item_stock.item_id = item.item_id
-WHERE item_stock.quantity = 0;
+WHERE item_stock.quantity = 0
+ORDER BY minutes_in_zero;
+
+-- 6. Quantities of items with paracetamol
+
+SELECT 
+    item_name, 
+    quantity
+FROM item_stock
+INNER JOIN item ON item_stock.item_id = item.item_id
+WHERE item_name LIKE '%mol' OR item_name LIKE 'para%';
