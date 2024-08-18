82b86b52a5c16239e6ce1a99079431bd6c95a421
Azie88 <obandoandrew8@gmail.com>
2024-08-18 12:42:58 +0300

    Queries Updated

--- Queries.sql
+++ Queries.sql
@@ -3,8 +3,8 @@
 SELECT 
     has_physical_inventory,
     AVG(price) AS average_price
-FROM item
-INNER JOIN item_price ON item.default_price_id = item_price.item_price_id
+FROM item_data
+INNER JOIN item_price ON item_data.default_price_id = item_price.item_price_id
 GROUP BY has_physical_inventory;
 
 -- 2. Show which storeroom has the most inventory.
@@ -20,37 +20,36 @@ ORDER BY total_quantity DESC
 LIMIT 1;
 
 -- 3. Time in minutes since item prices were last modified.
--- Difference between time of last item created vs. modified in item_prices table
+-- Difference between time of last item modified and current time in item_prices table
 
 SELECT 
     MAX(date_changed) AS last_date_changed,
-    MAX(date_created) AS last_date_created,
-    TIMESTAMPDIFF(MINUTE, MAX(date_created), MAX(date_changed)) AS minutes_since_last_change
+    TIMESTAMPDIFF(MINUTE, MAX(date_changed), NOW()) AS minutes_since_last_modified
 FROM item_price;
 
 -- 4. List items that are running out of stock.
+-- Items Less than 10 and more than 0
 
 SELECT 
-    item.item_id, 
-    item.item_name, 
+    item_data.item_id, 
+    item_data.item_name, 
     quantity
 FROM item_stock
-INNER JOIN item ON item_stock.item_id = item.item_id
+INNER JOIN item_data ON item_stock.item_id = item_data.item_id
 WHERE quantity > 0 AND quantity < 10
 ORDER BY quantity DESC;
 
 -- 5. List items whose quantity is Zero (0) and how long (In minutes) they have been in this state.
---  From last date changed to current date timestamp
+-- From last date changed to current date timestamp
 
 SELECT 
-    item.item_id,
-    item.item_name,
+    item_data.item_id,
+    item_data.item_name,
     item_stock.quantity,
-    date_created,
     date_changed,
-    TIMESTAMPDIFF(MINUTE, date_created, date_changed) AS minutes_in_zero
+    TIMESTAMPDIFF(MINUTE, date_changed, NOW()) AS minutes_in_zero
 FROM item_stock
-INNER JOIN item ON item_stock.item_id = item.item_id
+INNER JOIN item_data ON item_stock.item_id = item_data.item_id
 WHERE item_stock.quantity = 0
 ORDER BY minutes_in_zero;
 
@@ -60,5 +59,5 @@ SELECT
     item_name, 
     quantity
 FROM item_stock
-INNER JOIN item ON item_stock.item_id = item.item_id
-WHERE item_name LIKE '%mol' OR item_name LIKE 'para%';
+INNER JOIN item_data ON item_stock.item_id = item_data.item_id
+WHERE item_name LIKE 'paracetamol%';
